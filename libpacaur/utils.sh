df572c95552040383e30dc618be0c40b445f57a5
Ismael González <ismgonval@gmail.com>
2017-08-14 19:28:02 +0200

    Adapt commit d5bbdfe

--- libpacaur/utils.sh
+++ libpacaur/utils.sh
@@ -9,57 +9,54 @@
 # usage: Proceed( $default_option, $string )
 ##
 Proceed() {
-    local Y y N n answer
+    local Y y N n answer readline ret
     Y="$(gettext pacman Y)"; y="${Y,,}";
     N="$(gettext pacman N)"; n="${N,,}"
+    if [[ "$TERM" = dumb ]] || [[ $cleancache ]]; then
+        readline=1
+    else
+        readline=0
+    fi
     case "$1" in
         y)  printf "${colorB}%s${reset} ${colorW}%s${reset}" "::" "$2 [$Y/$n] "
-            if [[ ! $noconfirm ]]; then
-                case "$TERM" in
-                    dumb) # handle line buffering on dumb terminals
-                        read -r answer
-                        ;;
-                    *)
-                        if [[ $cleancache ]]; then
-                            read -r answer
-                        else
-                            read -r -n 1 answer
-                            echo
-                        fi
-                        ;;
-                esac
-            else
-                answer=$Y
+            if [[ $noconfirm ]]; then
                 echo
+                return 0
             fi
-            case $answer in
-                $Y|$y|'') return 0;;
-                *) return 1;;
-            esac;;
+            while true; do
+                if [[ $readline ]]; then
+                    read -r answer
+                else
+                    read -s -r -n 1 answer
+                fi
+                case $answer in
+                    $Y|$y|'') ret=0; break;;
+                    $N|$n) ret=1; break;;
+                    *) [[ $readline ]] && ret=1 && break;;
+                esac
+            done;;
         n)  printf "${colorB}%s${reset} ${colorW}%s${reset}" "::" "$2 [$y/$N] "
-            if [[ ! $noconfirm ]]; then
-                case "$TERM" in
-                    dumb) # handle line buffering on dumb terminals
-                        read -r answer
-                        ;;
-                    *)
-                        if [[ $cleancache ]]; then
-                            read -r answer
-                        else
-                            read -r -n 1 answer
-                            echo
-                        fi
-                        ;;
-                esac
-            else
-                answer=$N
+            if [[ $noconfirm ]]; then
                 echo
+                return 0
             fi
-            case $answer in
-                $N|$n|'') return 0;;
-                *) return 1;;
-            esac;;
+            while true; do
+                if [[ $readline ]]; then
+                    read -r answer
+                else
+                    read -s -r -n 1 answer
+                fi
+                case $answer in
+                    $N|$n|'') ret=0; break;;
+                    $Y|$y) ret=1; break;;
+                    *) [[ $readline ]] && ret=0 && break;;
+                esac
+            done;;
     esac
+    if ! [[ $readline ]]; then
+        echo "$answer"
+    fi
+    return $ret
 }
 
 ##
